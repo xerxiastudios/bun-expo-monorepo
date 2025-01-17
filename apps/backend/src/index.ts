5dcfe22b2d0772c7a87453c2e4cd481cb387dbe3
xerxiastudios <xerxiastudios@gmail.com>
2025-01-18 03:43:11 +0530

    Local apis with eden working

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,16 @@
+import { Elysia, t } from "elysia";
+import { cors } from "@elysiajs/cors";
+
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => "Hi Elysia")
+  .get("/id/:id", ({ params: { id } }) => id)
+  .post("/mirror", ({ body }) => body, {
+    body: t.Object({
+      id: t.Number(),
+      name: t.String(),
+    }),
+  })
+  .listen(3000);
+
+export type App = typeof app;
---
+++ apps/mobile/app/profile.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "~/components/ui/text";
+
+export default function Profile() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Profile Page</Text>
+    </View>
+  );
+}
